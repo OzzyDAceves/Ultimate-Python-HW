36902357aa98c296ecc02b6ac01fcb9204340b09
Héctor Ozzy Delgado Aceves <hectoraceves2000@gmail.com>
2023-10-31 00:10:50 -0600

    Actualitation

---
+++ Funtions/01-Functions.py
@@ -0,0 +1,9 @@
+def Hello(Name, Last_name="Mclovin"):
+    print("Hello world")
+    print(f"Welcome {Name} {Last_name}")
+
+
+Hello("Ozzy", "Aceves")
+Hello("Petu")
+
+Hello(Last_name="Aceves", Name="Ozzy")
